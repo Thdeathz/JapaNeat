e96780dce8fef7503c6f9b4d8ab45f03716f7c9b
Thdeathz <buidunga112@gmail.com>
2022-11-09 09:56:21 +0700

    fix: refactor video call using agora

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,37 +1,46 @@
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { db } from '~/firebase/config'
 
-const useFirestore = (collectionName, condition) => {
+const useFirestore = (collectionName, documentId, condition) => {
   const [documents, setDocuments] = useState([])
 
   useEffect(() => {
-    let collectionRef = collection(db, collectionName)
-    if (condition) {
-      if (!condition.compareValue || !condition.compareValue.length) {
-        // reset documents data
-        setDocuments([])
-        return
-      }
-      collectionRef = query(
-        collectionRef,
-        where(condition.fieldName, condition.operator, condition.compareValue)
-      )
+    let collectionRef
+    if (documentId) {
+      collectionRef = doc(db, collectionName, String(documentId))
     } else {
-      collectionRef = query(collectionRef, orderBy('createdAt', 'asc'))
+      collectionRef = collection(db, collectionName)
+      if (condition) {
+        if (!condition.compareValue || !condition.compareValue.length) {
+          // reset documents data
+          setDocuments([])
+          return
+        }
+        collectionRef = query(
+          collectionRef,
+          where(condition.fieldName, condition.operator, condition.compareValue)
+        )
+      } else {
+        collectionRef = query(collectionRef, orderBy('createdAt', 'asc'))
+      }
     }
-
     const unsubscribe = onSnapshot(collectionRef, snapshot => {
-      const res = snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-      }))
-
+      let res
+      if (documentId) {
+        res = snapshot.data()
+      } else {
+        res = snapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id
+        }))
+      }
+      console.log('=====> ref', res)
       setDocuments(res)
     })
 
     return unsubscribe
-  }, [collectionName, condition])
+  }, [collectionName, documentId, condition])
   return documents
 }
 
