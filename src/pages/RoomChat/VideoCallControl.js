97d0d9b959e5f72e470f35e1376805000dc7978d
Thdeathz <buidunga112@gmail.com>
2023-02-09 17:08:45 +0700

    fix: refactor room chat UI

---
+++ src/pages/RoomChat/VideoCallControl.js
@@ -0,0 +1,214 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { useNavigate, useParams } from 'react-router-dom'
+import { ReactMediaRecorder } from 'react-media-recorder'
+import { useDispatch } from 'react-redux'
+import { ButtonBase } from '@mui/material'
+import { v4 } from 'uuid'
+import { addDocument } from '~/firebase/services'
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked'
+import CircularProgress from '@mui/material/CircularProgress'
+import UploadFileIcon from '@mui/icons-material/UploadFile'
+import uploadFile from '~/hooks/uploadFile'
+import { useAddNewRecordMutation } from '../RecordDetail/recordsSlice'
+import { deleteRoomChat } from './roomChatSlice'
+import { FlexBetween } from '~/components'
+import {
+  CallEndOutlined,
+  FiberManualRecordOutlined,
+  InfoOutlined,
+  MessageOutlined,
+  MicOffOutlined,
+  MicOutlined,
+  VideocamOffOutlined,
+  VideocamOutlined
+} from '@mui/icons-material'
+
+function VideoCallControl({ client, tracks, videoData, roomData }) {
+  const { roomId, videoId } = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const [isWebcamActive, setIsWebcamActive] = useState(true)
+  const [isMicActive, setIsMicActive] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  const [addNewRecord] = useAddNewRecordMutation()
+
+  const handleOpenCamera = async () => {
+    if (tracks.length !== 0) {
+      console.log('=======> Tracks', tracks)
+      await tracks[1].setEnabled(!isWebcamActive)
+      setIsWebcamActive(!isWebcamActive)
+    }
+  }
+
+  const handleOpenMic = async () => {
+    if (tracks.length !== 0) {
+      await tracks[0].setEnabled(!isMicActive)
+      setIsMicActive(!isMicActive)
+    }
+  }
+
+  const handleLeaveRoom = async () => {
+    await client.leave()
+    client.removeAllListeners()
+    tracks[0].close()
+    tracks[1].close()
+    dispatch(
+      deleteRoomChat({
+        videoId: videoId,
+        roomId: roomId
+      })
+    )
+    navigate('/')
+  }
+
+  const handleUpload = async mediaBlobUrl => {
+    setLoading(true)
+    const blob = await fetch(mediaBlobUrl).then(r => r.blob())
+    if (blob) {
+      const file = new File([blob], String(v4()), { type: 'audio/wav' })
+      if (file) {
+        const path = await uploadFile(file)
+        if (path) {
+          try {
+            const res = await addNewRecord({
+              url: path,
+              thumbnail: videoData.video.thumbnail,
+              offer_id: roomData.offerId,
+              answer_id: roomData.answerId,
+              video_detail_id: videoData.id
+            }).unwrap()
+
+            await addDocument({
+              collectionName: 'notifications',
+              data: {
+                teacherId: videoData.teacher.id,
+                recordId: res.ids[0],
+                message: `${roomData.offerDisplayName} need your feedback !!!`
+              }
+            })
+
+            handleLeaveRoom()
+            setLoading(false)
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      }
+    }
+  }
+
+  return (
+    <FlexBetween className="basis-1/12 w-full relative" sx={{ justifyContent: 'center' }}>
+      <FlexBetween gap="0.75rem">
+        <ButtonBase
+          sx={{
+            backgroundColor: isMicActive ? 'rgba(71, 85, 105, 1)' : 'rgba(255, 0, 0, 1)',
+            p: '0.5rem',
+            borderRadius: '50%',
+            '&:hover': {
+              backgroundColor: isMicActive ? 'rgba(71, 85, 105, 0.5)' : 'rgba(255, 0, 0, 0.5)'
+            }
+          }}
+          onClick={handleOpenMic}
+        >
+          {isMicActive ? (
+            <MicOutlined sx={{ color: 'white' }} />
+          ) : (
+            <MicOffOutlined sx={{ color: 'white' }} />
+          )}
+        </ButtonBase>
+
+        <ButtonBase
+          sx={{
+            backgroundColor: isWebcamActive ? 'rgba(71, 85, 105, 1)' : 'rgba(255, 0, 0, 1)',
+            p: '0.5rem',
+            borderRadius: '50%',
+            '&:hover': {
+              backgroundColor: isWebcamActive ? 'rgba(71, 85, 105, 0.5)' : 'rgba(255, 0, 0, 0.5)'
+            }
+          }}
+          onClick={handleOpenCamera}
+        >
+          {isWebcamActive ? (
+            <VideocamOutlined sx={{ color: 'white' }} />
+          ) : (
+            <VideocamOffOutlined sx={{ color: 'white' }} />
+          )}
+        </ButtonBase>
+
+        <ReactMediaRecorder
+          screen
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+            <>
+              <ButtonBase
+                sx={{
+                  backgroundColor: 'rgba(71, 85, 105, 1)',
+                  p: '0.5rem',
+                  borderRadius: '50%',
+                  '&:hover': {
+                    backgroundColor: 'rgba(71, 85, 105, 0.5)'
+                  }
+                }}
+                onClick={() =>
+                  status === 'idle'
+                    ? startRecording()
+                    : status === 'recording'
+                    ? stopRecording()
+                    : handleUpload(mediaBlobUrl)
+                }
+              >
+                {status === 'idle' && <FiberManualRecordOutlined sx={{ color: 'white' }} />}
+                {status === 'recording' && <RadioButtonCheckedIcon sx={{ color: 'red' }} />}
+                {status === 'stopped' && (
+                  <>
+                    {loading ? (
+                      <CircularProgress color="textDefault" size={22} />
+                    ) : (
+                      <UploadFileIcon sx={{ color: 'white' }} />
+                    )}
+                  </>
+                )}
+              </ButtonBase>
+            </>
+          )}
+        />
+
+        <ButtonBase
+          sx={{
+            backgroundColor: 'rgba(255, 0, 0, 1)',
+            p: '0.5rem 1rem',
+            borderRadius: '1.5rem',
+            '&:hover': {
+              backgroundColor: 'rgba(255, 0, 0, 0.5)'
+            }
+          }}
+          onClick={handleLeaveRoom}
+        >
+          <CallEndOutlined sx={{ color: 'white' }} />
+        </ButtonBase>
+      </FlexBetween>
+
+      <FlexBetween className="absolute right-3" gap="0.25rem">
+        <ButtonBase sx={{ p: '0.5rem', borderRadius: '50%' }}>
+          <InfoOutlined sx={{ color: 'white' }} />
+        </ButtonBase>
+
+        <ButtonBase sx={{ p: '0.5rem', borderRadius: '50%' }}>
+          <MessageOutlined sx={{ color: 'white' }} />
+        </ButtonBase>
+      </FlexBetween>
+    </FlexBetween>
+  )
+}
+
+VideoCallControl.propTypes = {
+  client: PropTypes.object.isRequired,
+  tracks: PropTypes.array.isRequired,
+  videoData: PropTypes.object.isRequired,
+  roomData: PropTypes.object.isRequired
+}
+
+export default React.memo(VideoCallControl)
--- src/pages/RoomChat/index.js
+++ src/pages/RoomChat/index.js
@@ -1,37 +1,145 @@
-import { Box, Stack } from '@mui/material'
-import React from 'react'
-import ChatBox from './ChatBox'
-import VideoControl from './VideoControl'
-import VideoCall from './VideoCall'
+import { Box, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import { selectVideoById, useGetVideosQuery } from '../VideoDetail/videosSlice'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Loading } from '~/components'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { FlexBetween, Loading } from '~/components'
+import useFirestore from '~/hooks/useFirestore'
+import { APP_ID } from '~/agora/config'
+import { useClient, useMicrophoneAndCameraTracks } from '~/agora/rtc'
+import { AgoraVideoPlayer } from 'agora-rtc-react'
+import { toast } from 'react-toastify'
+import VideoCallControl from './VideoCallControl'
 
 export default function RoomChat() {
-  const { videoId } = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { roomId, videoId } = useParams()
   const { isLoading } = useGetVideosQuery()
 
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'))
   const video = useSelector(state => selectVideoById(state, videoId))
+  const roomData = useFirestore(`watchings/${videoId}/rooms`, roomId)
+
+  const client = useClient()
+  const { ready, tracks } = useMicrophoneAndCameraTracks()
+  const [remoteUser, setRemoteUser] = useState(null)
+
+  useEffect(() => {
+    let initRoom = async () => {
+      client.on('user-published', async (user, mediaType) => {
+        await client.subscribe(user, mediaType)
+        if (mediaType === 'video') {
+          setRemoteUser(user)
+        }
+        if (mediaType === 'audiox') {
+          user.audioTrack.play()
+        }
+      })
+
+      client.on('user-unpublished', (user, mediaType) => {
+        if (mediaType === 'audio') {
+          if (user.audioTrack) user.audioTrack.stop()
+        }
+        if (mediaType === 'video') {
+          setRemoteUser(null)
+        }
+      })
+
+      client.on('user-left', user => {
+        setRemoteUser(null)
+      })
+
+      await client.join(APP_ID, roomId, null, currentUser.id)
+      if (tracks) await client.publish([tracks[0], tracks[1]])
+    }
+
+    const leaveRoom = async () => {
+      await client.leave()
+      client.removeAllListeners()
+      tracks[0].close()
+      tracks[1].close()
+      navigate('/')
+    }
+
+    if (ready && tracks) {
+      initRoom()
+    }
+
+    if (!roomData) {
+      leaveRoom()
+      toast.info('Your Kaiwa session is over !!!', {
+        toastId: 1
+      })
+    }
+
+    return async () => leaveRoom
+  }, [videoId, roomId, ready, tracks, roomData])
 
   return (
     <>
       {isLoading ? (
         <Loading />
       ) : (
-        <Box className="flex justify-center">
-          <Box className="laptop:flex justify-center gap-4 py-2 laptop:h-[80vh] desktop:w-[90vw]">
-            <Box className="laptop:h-full basis-[70%]" color="secondary">
-              <VideoControl video={video} />
-            </Box>
-            <Box className="laptop:h-full basis-[25%]" color="secondary">
-              <VideoCall video={video} />
-            </Box>
-            <Box className="bg-secondary laptop:h-full h-[500px] basis-[30%]">
-              <ChatBox />
-            </Box>
+        <FlexBetween
+          flexDirection="column"
+          gap="1rem"
+          className="bg-slate-800 w-screen h-screen overflow-hidden p-4"
+        >
+          {console.log('===> remoteUser', remoteUser)}
+          <Box className="basis-11/12 w-full" sx={{ borderRadius: '0.5rem' }}>
+            <FlexBetween className="w-full h-full lg:flex-row flex-col" gap="1.5rem">
+              <FlexBetween alignItems="center" className="basis-3/4 h-full ">
+                <video className="w-full rounded-lg hover:cursor-pointer" controls>
+                  <source src={video.video.url} type="video/mp4" />
+                </video>
+              </FlexBetween>
+
+              <FlexBetween className="basis-1/4 w-full h-full lg:flex-col" gap="1rem">
+                <FlexBetween className="bg-white basis-1/2 w-full h-full rounded-lg relative">
+                  {ready && (
+                    <AgoraVideoPlayer
+                      id="video-call"
+                      videoTrack={tracks[1]}
+                      style={{ height: '100%', width: '100%', borderRadius: '0.5rem' }}
+                    />
+                  )}
+
+                  <Typography
+                    variant="h6"
+                    className="absolute bottom-0 left-2"
+                    sx={{ color: '#6aa6fa' }}
+                  >
+                    You
+                  </Typography>
+                </FlexBetween>
+
+                <FlexBetween className="bg-white basis-1/2 w-full h-full rounded-lg relative">
+                  {remoteUser?.videoTrack && (
+                    <AgoraVideoPlayer
+                      id="video-call"
+                      style={{ height: '100%', width: '100%' }}
+                      videoTrack={remoteUser.videoTrack}
+                    />
+                  )}
+
+                  <Typography
+                    variant="h6"
+                    className="absolute bottom-0 left-2"
+                    sx={{ color: '#6aa6fa' }}
+                  >
+                    {roomData?.offerId === currentUser.id
+                      ? roomData?.answerDisplayName
+                      : roomData?.offerDisplayName}
+                  </Typography>
+                </FlexBetween>
+              </FlexBetween>
+            </FlexBetween>
           </Box>
-        </Box>
+
+          <VideoCallControl client={client} tracks={tracks} videoData={video} roomData={roomData} />
+        </FlexBetween>
       )}
     </>
   )
